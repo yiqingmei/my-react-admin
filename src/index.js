4b6b833d9d5c84b6186d877ebcc9e7c6984bcdec
yiqingmei <yiqing.mei.life@gmail.com>
2024-11-12 18:05:33 +0800

    fix mockjs error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import store from './store'
 import { Provider } from 'react-redux';
+import './api/mock'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
